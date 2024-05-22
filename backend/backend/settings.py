d535a73bd2fecd4696cc95c18bfa4fcb2330a046
muyomba <muyombadavid148@gmail.com>
2024-05-22 14:32:49 +0300

    conect

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,11 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
+    'corsheaders',
 ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3002',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+  
 
 function App() {
+  const tableStyles = {
+    overflow: 'auto',
+    width: '100%',
+  };
+
+  const tableElementStyles = {
+    border: '1px solid #dededf',
+    height: '100%',
+    width: '100%',
+    tableLayout: 'fixed',
+    borderCollapse: 'collapse',
+    borderSpacing: '1px',
+    textAlign: 'left',
+  };
+
+  // const captionStyles = {
+  //   captionSide: 'top',
+  //   textAlign: 'left',
+  //  };
+
+  const thStyles = {
+    border: '1px solid #dededf',
+    backgroundColor: '#eceff1',
+    color: '#000000',
+    padding: '5px',
+  };
+
+  const tdStyles = {
+    border: '1px solid #dededf',
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    padding: '5px',
+  };
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+      axios.get('http://127.0.0.1:8000/api/account-holders/')
+          .then(response => {
+              setItems(response.data);
+          })
+          .catch(error => {
+              console.error('There was an error fetching the items!', error);
+          });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="table_component" role="region" tabIndex="0" style={tableStyles}>
+      <table style={tableElementStyles}>
+        
+        <thead>
+          <tr>
+            <th style={thStyles}>Account Number</th>
+            <th style={thStyles}>Account Name</th>
+            <th style={thStyles}>Available Balance</th>
+            <th style={thStyles}>Address</th>
+            <th style={thStyles}>Email</th>
+            <th style={thStyles}>Phone Number</th>
+          </tr>
+        </thead>
+        {items.map(item => (
+        <tbody key={item.id}>
+        <tr>
+            <td style={tdStyles}>{item.account_number}</td>
+            <td style={tdStyles}>{item.account_name}</td>
+            <td style={tdStyles}>{item.available_balance}</td>
+            <td style={tdStyles}>{item.address}</td>
+            <td style={tdStyles}>{item.email}</td>
+            <td style={tdStyles}>{item.phone_number}</td>
+          </tr>
+          
+        </tbody>
+        ))}
+      </table>
     </div>
+    
+    </>
   );
 }
 
